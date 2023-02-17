98e116d2c09f1ed1f0e376360b5511d8a441785b
Tanish Chaurasia <tanish.chaurasia@bonamisoftware.com>
2023-02-17 17:25:24 +0530

    Fix: Infomation for leadStatus added

--- crm-backend/prisma/seed.ts
+++ crm-backend/prisma/seed.ts
@@ -4,27 +4,56 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const ConfigData = {
-  leadSource: ['UpWork', 'LinkedIn', 'Google', 'SEO Tool', 'External Referral'],
+  leadSource: [
+    { label: 'UpWork ' },
+    { label: 'LinkedIn ' },
+    { label: 'Google ' },
+    { label: 'SEO Tool ' },
+    { label: 'External Referral' },
+  ],
 
   leadStatus: [
-    'Attempted to Contact',
-    'Contact in Future',
-    'In communication',
-    'Junk Lead',
-    'Lost Lead',
-    'Not Contacted',
-    'Pre-Qualified',
-    'Not Qualified',
-    'New Entry',
+    {
+      label: 'Attempted to Contact',
+      info: 'Lead is validated based  on their background, has indicated interest in what we have to offer and is otherwise more likely to become a customer.',
+    },
+    {
+      label: 'Contact in Future',
+      info: 'Lead which has a common scope of work and can now be converted into an active customer.',
+    },
+    {
+      label: 'In communication',
+      info: 'Sales qualified lead to which we have quoted budget and timelines.',
+    },
+    {
+      label: 'Junk Lead',
+      info: 'Lead which has mutually agreed to the terms and quotations, and the decision is pending.',
+    },
+    {
+      label: 'Lost Lead',
+      info: 'Lead which is ready for collaboration, documentation is pending.',
+    },
+    {
+      label: 'Not Contacted',
+      info: 'Lead with which we have closed the deal.',
+    },
+    {
+      label: 'Pre-Qualified',
+      info: 'Lead which has lost interest or not proceeding with our services.',
+    },
+    {
+      label: 'Unqualified',
+      info: 'Lead which does not qualify based on background, or has no interest in our services.',
+    },
   ],
 
   industry: [
-    'IT Services and Consulting',
-    'Software Development',
-    'Outsourcing',
-    'Government/Military',
-    'Large Enterprise',
-    'Hiring Platform',
+    { label: 'IT Services and Consulting' },
+    { label: 'Software Development' },
+    { label: 'Outsourcing' },
+    { label: 'Government/Military' },
+    { label: 'Large Enterprise' },
+    { label: 'Hiring Platform' },
   ],
 };
 
@@ -35,11 +64,12 @@ async function main() {
   while (current < configSeedData.length) {
     for await (const conf of ConfigData[configSeedData[current]]) {
       const config = await prisma.config.upsert({
-        where: { label: conf },
-        update: {},
+        where: { label: conf.label },
+        update: { information: conf.info },
         create: {
-          label: conf,
+          label: conf.label,
           type: configSeedData[current],
+          information: conf.info ? conf.info : null,
         },
       });
       console.log(config);
